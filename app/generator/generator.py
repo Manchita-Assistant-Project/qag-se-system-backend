bdf17189598ac269990778bb27ad4439593a1982
nklopstock611 <n.klopstock@uniandes.edu.co>
2024-10-16 22:19:41 -0500

    "manejo de errores" en qag

--- app/generator/generator.py
+++ app/generator/generator.py
@@ -1,7 +1,7 @@
 import app.generator.graph as graph
 
 if __name__ == "__main__":
-
+    
     # preguntas "Opción Múltiple"
     for i in range(1, 51):
         question_type = 1
@@ -10,7 +10,11 @@ if __name__ == "__main__":
         else:
             question_difficulty_int = 2
     
-        graph.use_graph(question_type, question_difficulty_int)
+        try:
+            graph.use_graph(question_type, question_difficulty_int)
+        except Exception as e:
+            print(f"Error al cargar la pregunta {i}: {e}")
+            continue
     
     # preguntas "Verdadero o Falso"
     for i in range(1, 51):
@@ -20,4 +24,8 @@ if __name__ == "__main__":
         else:
             question_difficulty_int = 2
     
-        graph.use_graph(question_type, question_difficulty_int)
+        try:
+            graph.use_graph(question_type, question_difficulty_int)
+        except Exception as e:
+            print(f"Error al cargar la pregunta {i}: {e}")
+            continue
